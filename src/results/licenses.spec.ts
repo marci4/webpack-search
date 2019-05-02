4e0c4f8492cedf0110e2e566ced3e913ef71bd89
marci4 <1-marci4@marci4.de>
2019-05-02 17:53:02 +0000

    Resolve "Update license"

--- src/results/licenses.spec.ts
+++ src/results/licenses.spec.ts
@@ -1,3 +1,11 @@
+/*
+ Copyright (c) 2019 Marcel Prestel
+ "THE BEER-WARE LICENSE" (Revision 42):
+ <admin@marci4.de> wrote this file. As long as you retain this notice you
+ can do whatever you want with this stuff. If we meet some day, and you think
+ this stuff is worth it, you can buy me a beer in return.
+ */
+
 import {Licenses} from "./licenses";
 
 describe("Licenses", () => {
