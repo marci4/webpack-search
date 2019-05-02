491a3af78c5c690ba0a4c621f985f7f83de7441e
marci4 <1-marci4@marci4.de>
2019-05-02 17:21:15 +0000

    Resolve "Fix tests"

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -5,15 +5,21 @@ function mockExitImplementation(code?: number): never {
 	throw new Error("" + code);
 }
 
+function mockConsoleImplementation(): void {
+	// Ignore
+}
+
 describe("Main", () => {
 	it("static main with missing arguments", (done) => {
 		const mockExit = jest.spyOn(process, "exit").mockImplementation(mockExitImplementation);
+		const mockError = jest.spyOn(console, "error").mockImplementation(mockConsoleImplementation);
 		try {
 			main(["hello"]).catch(() => done());
 		} catch (e) {
 			expect(e).toEqual("1");
 		}
 		expect(mockExit).toHaveBeenCalledWith(1);
+		expect(mockError).toHaveBeenCalledTimes(2);
 		done();
 	});
 });
